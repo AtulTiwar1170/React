e36f8bc9a5d1db9dd1a95d89c17ff79f147edfe4
AtulTiwar1170 <atult2620@gmail.com>
2024-07-06 21:55:41 +0530

    using api with useEffect

--- day2/src/App.jsx
+++ day2/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import { useEffect, useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [apidata, setApidata] = useState([])
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(response => response.json())
+    .then(data => setApidata(data))  
+  }, []);
 
+  
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='px-40 py-20 space-y-5'>
+      <h1 className='text-3xl text-blue-500 font-bold'>Comment with email</h1>
+        {
+          apidata.map( comments => {
+            return <div className='px-5 py-3 bg-zinc-100 rounded-lg' key={comments.id}>
+              <h4>Name : {comments.name}</h4>
+              <h4>Email : {comments.email}</h4>
+              <h4>Comment : {comments.body}</h4>
+            </div>
+          })
+        }
+    </div>
   )
 }
 
